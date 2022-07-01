143ed3e003fe5f34a5deb7d07b2269b22909ad84
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-01 14:54:31 +0300

    fix: add like button

--- src/main.js
+++ src/main.js
@@ -22,7 +22,8 @@ const renderStation = (search) => {
   const clickcount = seacrh.clickcount;
   console.log(radioUrl);
 };
-//
+//funtion to render all the stations on load
+
 async function getStations() {
   const users = await fetch(
     "https://at1.api.radio-browser.info/json/stations/byname/station"
