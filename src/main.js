18fea421589f5ead4261112b4e73508700be5be5
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-01 14:38:48 +0300

    fix:change scroll direction of radio stations

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => {
+  getStations().then((data) => console.log(data));
+});
 
-function init(){
-	fetchData()
-}
+// function init() {
+//   fetchData();
 
 const fetchData = (station = "Classic 105") => {
-	
-fetch(`https://at1.api.radio-browser.info/json/stations/byname/${station}`)
-.then(res => res.json())
-.then(data => data.forEach(item => renderStation(item)))
-.catch(err => console.error(err));
-
-}
+  fetch(`https://at1.api.radio-browser.info/json/stations/byname/${station}`)
+    .then((res) => res.json())
+    .then((data) => data.forEach((item) => renderStation(item)))
+    .catch((err) => console.error(err));
+};
 
 const renderStation = (search) => {
   const radioUrl = search.url;
@@ -21,5 +20,12 @@ const renderStation = (search) => {
   const format = search.codec;
   const votes = search.votes;
   const clickcount = seacrh.clickcount;
-  console.log(radioUrl)
+  console.log(radioUrl);
+};
+//
+async function getStations() {
+  const users = await fetch(
+    "https://at1.api.radio-browser.info/json/stations/byname/station"
+  );
+  return users.json();
 }
