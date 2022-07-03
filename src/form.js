eae058c7e7e8cc52bb3d824d6a2b589ac32662a8
Bonface Masira <bonfacemasira@gmail.com>
2022-07-03 03:24:16 +0300

    prevent default form behavior and fetch stations

--- src/form.js
+++ src/form.js
@@ -1,10 +1,18 @@
 const search = () =>{
     let searchForm =document.querySelector('form');
-    console.log(searchForm);
 
     searchForm.addEventListener('submit', (event) =>{
         event.preventDefault();
-    })
+
+        const input = document.querySelector('input.search')
+        console.log(input.value)
+
+        fetch(`https://at1.api.radio-browser.info/json/stations/byname/${input.value}`)
+        .then((resp) => resp.json())
+        .then(data => {
+            console.log(data);
+        });
+    });
 }
 
 
